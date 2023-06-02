bc8508972bcf5d8d65af9e221f7b7bc986642ef3
Rogerio <rogeryo.noturno@gmail.com>
2023-06-02 16:59:53 -0300

    Aula 06

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`A velocidade  do seu carro é ${vel}Km/h`)
+if (vel > 60) {// Condição simples!
+    console.log(`Você ultrapasspou o limite de velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dritija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Kenya'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else [
+    console.log('Vocêr é Estrangeiro!')
+]
