ace3f330c6137ef3a7568839997ca6bb0685f921
Rogerio <rogeryo.noturno@gmail.com>
2023-06-20 16:54:25 -0300

    Repetiçoes While, Do While

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+
+
+
+
+ 
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
